972cad525f5eec8f9e878277e116f7972fdedb5f
kominsok <kominsok@naver.com>
2023-03-03 13:50:01 +0900

    test 06 추가

---
+++ src/Test06.java
@@ -0,0 +1,17 @@
+public class Test06 {
+    
+    public void run(){
+        
+    }
+
+    /*
+     * "hello"	"ohell"	1
+    "apple"	"elppa"	-1
+    "atat"	"tata"	1
+    "abc"	"abc"	0
+     */
+    public int solution(String A, String B) {
+        int answer = 0;
+        return answer;
+    }
+}
