8f7d24af3f88d40e85470281cde9cb7344192ba1
kominsok <kominsok@naver.com>
2023-03-08 15:28:12 +0900

    프로그래머스 6장

--- src/Test06.java
+++ src/Test06.java
@@ -1,7 +1,15 @@
 public class Test06 {
     
     public void run(){
-        
+        String A = "hello";
+        String B = "ohell";
+
+        System.out.println(this.solution(A, B));
+
+        A = "apple";
+        B = "elppa";
+
+        this.solution(A, B);
     }
 
     /*
@@ -12,6 +20,26 @@ public class Test06 {
      */
     public int solution(String A, String B) {
         int answer = 0;
+        
+        if (A.equals(B)){
+            return answer;
+        }
+
+        String tmpA = A;
+        for (int i = 0; i < A.length(); i++){
+            String rightChar = tmpA.substring(tmpA.length() - 1, tmpA.length());
+            String subChar = tmpA.substring(0, tmpA.length() - 1);
+
+            tmpA = rightChar + subChar;
+
+            //System.out.println(tmpA);
+
+            if (tmpA.equals(B)){
+                return (i + 1);
+            }
+        }
+
+        answer = -1;
         return answer;
     }
 }
